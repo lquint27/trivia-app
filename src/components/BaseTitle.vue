ec165499eb1431cddc9a838f854e22111bd3663c
Luis Quintanilla <lquint27@southtexascollege.edu>
2025-10-07 20:28:20 -0500

    BaseTitle Working

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex items-center justify-center gap-4">
+        <h1 class="text-6xl font-thin text-slate">
+            <slot></slot>
+        </h1>
+
+    </div>
+</template>
