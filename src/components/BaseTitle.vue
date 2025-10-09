8bd65e06773f396e6b30de99ab1dc07af0db73a3
Luis Quintanilla <lquint27@southtexascollege.edu>
2025-10-08 20:15:08 -0500

    Added style to categories

--- src/components/BaseTitle.vue
+++ src/components/BaseTitle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center justify-center gap-4">
-        <h1 class="text-6xl font-thin text-slate">
+        <h1 class="text-6xl font-thin text-white uppercase">
             <slot></slot>
         </h1>
 
