ced65194a9c0a95b717a5b7112e5c55c405c69b6
Luis Quintanilla <lquint27@southtexascollege.edu>
2025-10-19 22:11:04 -0500

    shuffle is working

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -7,7 +7,7 @@ import BaseTitle from '@/components/BaseTitle.vue';
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
-const answer = ref([])
+const answers = ref([])
 
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
@@ -26,7 +26,21 @@ onMounted(async () => {
       answers: wrong_answer
     } )
   })
+
+  answers.value = shuffle(answers.value)
+  //console.log(question.value)
+
 })
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * (i + 1));
+    //swap
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array
+}
 
 </script>
 
